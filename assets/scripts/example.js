625255d7517072b0fce3d7452de380c8be4b3073
rachael miller design <rachael@rachaelmillerdesign.com>
2018-06-22 11:02:24 -0400

    attempting to create x and o tokens

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,4 +1,14 @@
-const element = document.createElement('div')
+/* const element = document.createElement('div')
 element.id = 'myDiv'
 element.innerHTML = 'Hello World!'
-document.body.appendChild(element)
+document.body.appendChild(element) */
+
+function boardclick () {
+  this.innerHTML = boardclick.token
+  boardclick.token = boardclick.token === 'X' ? 'O' : 'X'
+}
+boardclick.token = 'X'
+
+for (let i = 1; i < 10; ++i) {
+  document.getElementById('cell' + i).onClick = boardclick
+}
