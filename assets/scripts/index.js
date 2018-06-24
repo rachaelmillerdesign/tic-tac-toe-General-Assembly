b6718e432a4a6f9148717178d306c60ef5ea3235
rachael miller design <rachael@rachaelmillerdesign.com>
2018-06-24 12:47:36 -0400

    cells will now show 'x' when clicked, added id's for cells

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,8 +4,54 @@
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
-// require('./example')
-
+//require('./example')
+/*
 $(() => {
-  // your JS code goes here
+  }
 })
+
+let currentPlayer = [i]
+for (let i=0; i <emptyBoard.length; i++) {
+when player clicks, record location and toggle to player2
+}
+
+/*
+const changePlayer = function (click) {
+    event.preventDefault()
+//need to create class square-x and square-o
+    if (currentPlayer === 'x') {
+      $(this).toggleClass('square-x')
+    } else {
+      $(this).toggleClass('square-o')
+    }
+  }
+}) */
+
+
+
+$('#one').click(function () {
+  $('#one').text('x')
+})
+
+const onClick = function () {
+  console.log('!')
+}
+$('.square').on('click', onClick)
+
+/*
+function makeMove(square, player) {
+  var marker = document.createElement('div');
+  /* set the class name on the new div to X-marker or O-marker, depending on the current player
+  marker.className = player + "-marker";
+  square.appendChild(marker) */
+ /*
+let squareId = ['square one', 'square two', 'square three',
+  'square four', 'square five', 'square six' ,
+  'square seven' , 'square eight', 'square nine' ]
+
+const emptyBoard = [ [ 'square one', 'square two', 'square three' ],
+  [ 'square four', 'square five', 'square six' ],
+  [ 'square seven', 'square eight', 'square nine' ]
+]
+*/
+//any square class needs to equal squareId
