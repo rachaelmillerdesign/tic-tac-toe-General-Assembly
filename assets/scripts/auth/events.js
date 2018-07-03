d30123cf438f494c94db5a0c1fa317b418ae8b89
rachael miller design <rachael@rachaelmillerdesign.com>
2018-07-03 14:03:13 -0400

    changing approach: will use non modal screens.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,11 +1,21 @@
-$('#myModal').modal(console.log('I am here!'))
+/$('#myModal').modal(console.log('I am here!'))
+// Get the modal
+const modal = document.getElementById('id01')
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none'
+  }
+}
 // let modal = document.getElementById('myModal')
 // const btn = document.getElementById('myBtn')
 //
 // btn.onclick = function () {
 //   modal.style.display = 'block'
 // }
+
+// $('#someIdOnYourPage').text('you won')
 
 const getFormFields = require('../../../lib/get-form-fields')
 
